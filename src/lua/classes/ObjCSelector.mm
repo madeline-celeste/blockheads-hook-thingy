0d663674493934edd0ceb935aa16827d3de29c3f
madeline-celeste <madeline-celeste@users.noreply.github.com>
2025-08-29 13:05:42 -0400

    implement ObjCSelector__tostring

--- src/lua/classes/ObjCSelector.mm
+++ src/lua/classes/ObjCSelector.mm
@@ -6,7 +6,14 @@
 #import <Foundation/Foundation.h>
 
 
-int ObjCSelector__tostring(lua_State* L) {}
+static int ObjCSelector__tostring(lua_State* L) {
+    SEL sel = *(SEL*)luaL_checkudata(L, 1, "ObjCSelector");
+
+    NSString* fancyName = [NSString stringWithFormat:@"%s<%@>", "ObjCSelector", NSStringFromSelector(sel)];
+    pushNSString(L, fancyName);
+
+    return 1;
+}
 
 int pushObjCSelector(lua_State* L, SEL sel) {
     SEL* udata = (SEL*)lua_newuserdata(L, sizeof(SEL));
@@ -21,6 +28,9 @@ int pushObjCSelector(lua_State* L, SEL sel) {
 void registerObjCSelector(lua_State* L) {
     luaL_newmetatable(L, "ObjCSelector");
 
+
+    lua_pushcfunction(L, ObjCSelector__tostring);
+    lua_setfield(L, -2, "__tostring");
     lua_pushvalue(L, -1);
     lua_setfield(L, -2, "__index");
 
