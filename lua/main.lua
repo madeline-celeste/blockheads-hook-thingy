fa737e7a7a31231e35e89b462d5564e8770795b4
madeline-celeste <madeline-celeste@users.noreply.github.com>
2025-08-29 13:53:29 -0400

    update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -24,28 +24,25 @@ hook_objc("BHNetNode", "initWithDelegate:match:netNodeType:", function(self, _cm
     return nil
 end)
 
+
+-- some stuff to mess around cuz why not
 hook_objc("World", "welcomeMessage", function(self, _cmd)
     local bhServer = GameController:get("bhServer")
 
     return bhServer:getDebugLog()
 end)
-
 hook_objc("BHServer", "playerIsCloudWideAdminWithAlias:", function(self, _cmd)
     return true -- EVIL: make everyone have cool red names and power of doom
 end)
-
 hook_objc("ServerClient", "addFillRequest:", function(self, _cmd, fillIndex)
     print("fillIndex:", fillIndex)
     GameController:get("bhServer"):bootAllClientsDueToNoCredit()
 end)
-
 hook_objc("BHMatch", "setDelegate:", function(self, _cmd, delegate)
     print("BHMatch SETDELEGATE")
     print(delegate:className())
     abort()
 end)
-
-
 hook_objc("BHNetServerMatch", "isCloudMatch", function(self, _cmd)
     -- if return true, everyone will be kicked because of there being -1.0 credit lmao
     return false
@@ -58,3 +55,9 @@ end)
 
 --list_methods(GameController)
 --list_methods(GameController:get("bhServer"))
+
+function printClassesList()
+    print(table.concat(ObjCDebug.getClassList(), "\n"))
+end
+
+printClassesList()
